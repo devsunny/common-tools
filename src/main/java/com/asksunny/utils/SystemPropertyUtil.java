bed3b3b83ea93bb506c397571f994919bf4961cf
devsunny <sunnyliu2@gmail.com>
2015-11-12 20:18:09 -0500

    Commit

--- src/main/java/com/asksunny/utils/SystemPropertyUtil.java
+++ src/main/java/com/asksunny/utils/SystemPropertyUtil.java
@@ -1,5 +1,6 @@
 package com.asksunny.utils;
 
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,6 +24,25 @@ public final class SystemPropertyUtil {
 		matcher.appendTail(buf);
 		return buf.toString();
 	}
+	
+	
+	public static String searchAndReplace(CharSequence text, Map<String, String> props) {
+		Matcher matcher = SYS_PROP_PATTERN.matcher(text);
+		StringBuffer buf = new StringBuffer();
+		while (matcher.find()) {
+			String key = matcher.group(1);
+			String value = props.get(key);
+			if (value != null) {
+				if (SYS_PROP_PATTERN.matcher(value).find()) {
+					value = searchAndReplace(value, props);
+				}
+				matcher.appendReplacement(buf, value);
+			}
+		}
+		matcher.appendTail(buf);
+		return buf.toString();
+	}
+	
 
 	private SystemPropertyUtil() {		
 	}
